557c06a0ca718805ac8650b43252c39a30bf6884
ydjdgm <tonytony060720@gmail.com>
2025-02-05 23:29:15 +0100

    Update Navbar.svelte

--- myapp/src/routes/Navbar.svelte
+++ myapp/src/routes/Navbar.svelte
@@ -9,92 +9,96 @@
 
 <div class="navbar {isOpen ? '' : 'collapsedNavbar'}">
 	<button class="toggleBtn" on:click={toggleNavbar}>
-		{isOpen ? 'x' : '☰'}
+		{isOpen ? 'X' : '☰'}
 	</button>
-	<img class="logo" src="/starrail_logo.png" alt="Starrail Logo" />
-	<ul class="menuList">
-		<li class = "menuElement"><a href="/"><img class="navIcon {isOpen ? '' : 'collapsedNavIcon'}" src="/navbar/home.png" alt="Home"/>Home</a></li>
-		<li class = "menuElement"><a href="/char"><img class="navIcon {isOpen ? '' : 'collapsedNavIcon'}"src="/navbar/char.png"alt="Char"/>Characters</a></li>
-		<li class = "menuElement"><a href="/lightcone"><img class="navIcon {isOpen ? '' : 'collapsedNavIcon'}" src="/navbar/lightcone.png" alt="Lightcone"/>Lightcones</a></li>
-		<li class = "menuElement"><a href="/relic"><img class="navIcon {isOpen ? '' : 'collapsedNavIcon'}" src="/navbar/relic.png" alt="Relic"/>Relic</a></li>
+	<a class="logoA {isOpen ? '' : 'collapsedLogoA'}" href="/"><img src="/starrail_logo.png" alt="Starrail Logo" /></a>
+	<ul class="menuLi">
+		<li class="menuEle"><a href="/"><button class="eleABtn"><img class="eleABtnImg {isOpen ? '' : 'collapsedEleABtnImg'}" src="/navbar/home.png" alt="home"><h1 class="eleABtnH1 {isOpen ? '' : 'collapsedEleABtnH1'}">Home</h1></button></a></li>
+		<li class="menuEle"><a href="/char"><button class="eleABtn"><img class="eleABtnImg {isOpen ? '' : 'collapsedEleABtnImg'}" src="/navbar/char.png" alt="char"><h1 class="eleABtnH1">Character</h1></button></a></li>
+		<li class="menuEle"><a href="/lightcone"><button class="eleABtn"><img class="eleABtnImg {isOpen ? '' : 'collapsedEleABtnImg'}" src="/navbar/lightcone.png" alt="lightcone"><h1 class="eleABtnH1">Lightcone</h1></button></a></li>
+		<li class="menuEle"><a href="/relic"><button class="eleABtn"><img class="eleABtnImg {isOpen ? '' : 'collapsedEleABtnImg'}" src="/navbar/relic.png" alt="relic"><h1 class="eleABtnH1">Relic</h1></button></a></li>
+		<!-- <li class = "menuElement"><a class="navA" href="/"><button class="navButton {isOpen ? '' : 'collapsedNavButton'}"><img class="navIcon {isOpen ? '' : 'collapsedNavIcon'}" src="/navbar/home.png" alt="home">Home</button></a>
+		<li class = "menuElement"><a class="navA" href="/char"><button class="navButton"><img class="navIcon {isOpen ? '' : 'collapsedNavIcon'}" src="/navbar/char.png" alt="char">Character</button></a>
+		<li class = "menuElement"><a class="navA" href="/lightcone"><button class="navButton"><img class="navIcon {isOpen ? '' : 'collapsedNavIcon'}" src="/navbar/lightcone.png" alt="lightcone">Lightcone</button></a>
+		<li class = "menuElement"><a class="navA" href="/relic"><button class="navButton"><img class="navIcon {isOpen ? '' : 'collapsedNavIcon'}" src="/navbar/relic.png" alt="relic">Relic</button></a> -->
 	</ul>
 </div>
 
 <style>
 	.navbar {
+		position: fixed;
 		width: 13vw;
-		max-width: 250px;
 		height: 100vh;
+		display: block;
+		justify-content: center;
+		top: 0;
 		overflow-y: auto;
 		scrollbar-width: none;
-		background: #0c0c1f;
-		color: white;
-		position: fixed;
-		top: 0;
-		left: 0;
+		background: #010131;
+
 		transition: transform 0.3s ease;
 		transform: translateX(0);
-	}
-	@media (max-width: 768px) {
-		.navbar {
-			width: 25vw;
-			min-width: 140px;
-		}
-	}
-	@media (max-width: 480px) {
-		.navbar {
-			width: 30vw;
-			min-width: 120px;
-		}
-	}
-
-	.collapsedNavbar {
+	}.collapsedNavbar {
 		transform: translateX(-80%);
 	}
 
-    .logo {
-		position: relative;
-        right: 20px;
-        bottom: 40px;
-		width: 180px;
-		margin: 20px auto;
+	.toggleBtn {
+		position: absolute;
+		width: 10%;
+		height: 4%;
+		top: 1%;
+		right: 5%;
+		/* background: transparent; */
+		background-color: rgb(27, 107, 177);
+		color: white;
+		z-index: 1;
+		font-size: 1vw;
+	}
+
+	.logoA{
+		display: block;
+		margin: 0 auto;
+		width: 11vw;
+		height: 20vh;
+		visibility: visible;
+		opacity: 1;
+		transition: opacity 0.3s ease;
+	}.collapsedLogoA{
+		visibility: hidden;
+		opacity: 0;
+		transition: visibility 0s 0.3s, opacity 0.3s ease;
+	}
+
+	.menuLi{
 		display: block;
 	}
 
-	.toggleBtn {
-		width: 40px;
-		height: 40px;
-		position: absolute;
-		top: 10px;
-		right: 10px;
-		background: rgb(188, 188, 206);
-		border: none;
-		color: white;
-		padding: 5px 10px;
-		cursor: pointer;
-		border-radius: 4px;
+	.menuEle{
+		display: flex;
 	}
 
-    .menuList {
-        position: relative;
-        bottom: 80px;
-        left: 45px;
-    }
+	.eleABtn{
+		display: flex;
+		width: 13vw;
+		background-color: rgb(72, 100, 100);
+	}
 
-    .menuElement {
-        margin: 10px;
-        margin-bottom: 30px;
-    }
-
-	.navIcon {
-		position: absolute;
-		width: 30px;
-		height: 30px;
-        right: 255px;
+	.eleABtnImg{
+		width: 3vw;
 		transition: transform 0.3s ease;
+	}.collapsedEleABtnImg{
+		transform: translateX(10vw) translateY(-3vh);
 	}
 
-	.collapsedNavIcon {
-		transform: translateX(200px) translateY(-80px);
+	.eleABtnH1{
+		font-size: 1.5vw;
+		color: white;
+		visibility: visible;
+		opacity: 1;
+		transition: opacity 0.3s ease;
+	}.collapsedEleABtnH1{
+		visibility: hidden;
+		opacity: 0;
+		transition: visibility 0s 0.3s, opacity 0.3s ease;
 	}
 </style>
